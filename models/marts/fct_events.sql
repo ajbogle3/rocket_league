06be1db26af9b7bfd065e3f3dd895f1646b77f4d
bogle.adrian@protonmail.com <bogle.adrian@protonmail.com>
2022-05-03 17:47:06 +0000

    added team_id and game_duration to fct_events

--- models/marts/fct_events.sql
+++ models/marts/fct_events.sql
@@ -1,5 +1,23 @@
 with events as (
     select * from {{ ref('int_event_history') }}
+),
+
+matches as (
+    select * from {{ ref('int_team_match_history') }}
+),
+
+final as (
+    select
+        events.event_id,
+        matches.match_id,
+        matches.team_id,
+        events.game_id,
+        events.game_duration,
+        events.event_phase,
+        events.game_OT,
+        events.map_id
+    from events
+    left join matches using (match_id)
 )
 
-select * from events
+select * from final
--- models/staging/events/stg_event_details.sql
+++ models/staging/events/stg_event_details.sql
@@ -4,6 +4,7 @@ with events_details as (
         event_id,
         match_id,
         game_id,
+        game_duration::int as game_duration,
         event_phase,
         overtime as game_OT,
         map_id
